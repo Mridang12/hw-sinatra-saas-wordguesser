e3644813012ffe7eaccceaa7af9cb03ad577bb7c
Mridang Sheth <mridangs@gmail.com>
2023-01-06 20:59:10 +0530

    finished part 1

--- lib/wordguesser_game.rb
+++ lib/wordguesser_game.rb
@@ -7,6 +7,55 @@ class WordGuesserGame
 
   def initialize(word)
     @word = word
+    @guesses = ""
+    @wrong_guesses = ""
+  end
+
+  attr_accessor :word
+  attr_accessor :guesses
+  attr_accessor :wrong_guesses
+
+  def guess(letter)
+    if letter == nil || letter.length != 1
+      raise ArgumentError.new("Can only guess single character")
+    end
+    
+    if !(letter =~ /[A-Za-z]/)
+      raise ArgumentError.new("Guess can only be a valid english alphabet character")
+    end
+
+    if @guesses.downcase.include?(letter.downcase) || @wrong_guesses.downcase.include?(letter.downcase)
+      return false
+    end
+
+    if @word.downcase.include?(letter.downcase)
+      @guesses += letter
+    else
+      @wrong_guesses += letter
+    end
+    return true
+  end
+
+  def word_with_guesses
+    display_word = ""
+    @word.split('').each { |c| 
+      if @guesses.include?(c)
+        display_word += c
+      else
+        display_word += "-"
+      end
+    }
+    return display_word
+  end
+
+  def check_win_or_lose
+    if @word == self.word_with_guesses
+      return :win
+    elsif @wrong_guesses.length >= 7
+      return :lose
+    else
+      return :play
+    end
   end
 
   # You can test it by installing irb via $ gem install irb
