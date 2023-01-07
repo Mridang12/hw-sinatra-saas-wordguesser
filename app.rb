3c73c9f0f1200e723158da0e929d3920040a1dcc
Mridang Sheth <mridangs@gmail.com>
2023-01-07 15:17:06 +0530

    ready for deployment

--- app.rb
+++ app.rb
@@ -40,6 +40,13 @@ class WordGuesserApp < Sinatra::Base
   post '/guess' do
     letter = params[:guess].to_s[0]
     ### YOUR CODE HERE ###
+    begin
+      if !@game.guess(letter)
+        flash[:message] = "You have already used that letter."
+      end
+    rescue => exception
+      flash[:message] = "Invalid guess."
+    end
     redirect '/show'
   end
   
@@ -50,6 +57,13 @@ class WordGuesserApp < Sinatra::Base
   # wrong_guesses and word_with_guesses from @game.
   get '/show' do
     ### YOUR CODE HERE ###
+    outcome = @game.check_win_or_lose
+    if outcome == :lose
+      redirect '/lose'
+    elsif outcome == :win
+      redirect '/win'
+    end
+
     erb :show # You may change/remove this line
   end
   
